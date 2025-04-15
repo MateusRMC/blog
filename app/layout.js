0c06931f2d4f9cfbcd904f6b0b9bf47ee901f20b
Mateus Rocha <155510055+MateusRMC@users.noreply.github.com>
2025-04-15 13:47:03 -0400

    gtm com head

--- app/layout.js
+++ app/layout.js
@@ -4,13 +4,13 @@ import { GeistMono } from "geist/font/mono";
 export default function HomeLayout({ children }) {
   return (
     <html lang="en">
-      <!-- Google Tag Manager -->
+      <head>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5MXJ8CP2');</script>
-<!-- End Google Tag Manager -->
+</head>
       <body>{children}</body>
     </html>
   );
